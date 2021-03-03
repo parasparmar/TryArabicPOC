780d4e219e560c2a156380d1773a9eb6f043ae1b
Paras Parmar <parasparmar@gmail.com>
2021-03-03 11:59:34 +0530

    Add project files.

---
+++ TryArabicPOC/Models/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TryArabicPOC.Models
+{
+    public class Menu
+    {
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public Menu ChildMenu { get; set; }
+
+    }
+}
