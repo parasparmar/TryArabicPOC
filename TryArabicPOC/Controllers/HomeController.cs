ec97d20777bba8c4470a90762f157ace88582452
Paras Parmar <parasparmar@gmail.com>
2021-03-04 15:25:33 +0530

    Works Now.

--- TryArabicPOC/Controllers/HomeController.cs
+++ TryArabicPOC/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using i18n;
+using i18n.Helpers;
 
 namespace TryArabicPOC.Controllers
 {
@@ -25,6 +27,46 @@ namespace TryArabicPOC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult SetLanguage(string langtag, string returnUrl)
+        {
+            // If valid 'langtag' passed.
+            i18n.LanguageTag lt = i18n.LanguageTag.GetCachedInstance(langtag);
+            if (lt.IsValid())
+            {
+                // Set persistent cookie in the client to remember the language choice.
+                Response.Cookies.Add(new HttpCookie("i18n.langtag")
+                {
+                    Value = lt.ToString(),
+                    HttpOnly = true,
+                    Expires = DateTime.UtcNow.AddYears(1)
+                });
+            }
+            // Owise...delete any 'language' cookie in the client.
+            else
+            {
+                var cookie = Response.Cookies["i18n.langtag"];
+                if (cookie != null)
+                {
+                    cookie.Value = null;
+                    cookie.Expires = DateTime.UtcNow.AddMonths(-1);
+                }
+            }
+            // Update PAL setting so that new language is reflected in any URL patched in the 
+            // response (Late URL Localization).
+            HttpContext.SetPrincipalAppLanguageForRequest(lt);
+            // Patch in the new langtag into any return URL.
+            if (returnUrl.IsSet())
+            {
+                if (LocalizedApplication.Current.UrlLocalizerForApp.FilterOutgoing(returnUrl, HttpContext.Request.Url)) // if url wants to be localized
+                {
+                    returnUrl = LocalizedApplication.Current.UrlLocalizerForApp.SetLangTagInUrlPath(HttpContext, returnUrl, UriKind.RelativeOrAbsolute, lt?.ToString()).ToString();
+                }
+            }
+            // Redirect user agent as approp.
+            return this.Redirect(returnUrl);
         }
     }
 }
--- TryArabicPOC/Global.asax.cs
+++ TryArabicPOC/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using i18n;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,79 @@ namespace TryArabicPOC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Change from the default of 'en'.
+            i18n.LocalizedApplication.Current.DefaultLanguage = "en";
+
+            // Change from the default of 'i18n.langtag'.
+            i18n.LocalizedApplication.Current.CookieName = "i18n_langtag";
+
+            // Change from the of temporary redirects during URL localization
+            i18n.LocalizedApplication.Current.PermanentRedirects = true;
+
+            // This line can be used to disable URL Localization.
+            //i18n.UrlLocalizer.UrlLocalizationScheme = i18n.UrlLocalizationScheme.Void;
+
+            // Change the URL localization scheme from Scheme1.
+            i18n.UrlLocalizer.UrlLocalizationScheme = i18n.UrlLocalizationScheme.Scheme2;
+
+            // Change i18n's expectation for the ASP.NET application's virtual application root path on the server, 
+            // used by Url Localization. Defaults to "/".
+            //i18n.LocalizedApplication.Current.ApplicationPath = "/mysite";
+
+            // Specifies whether the key for a message may be assumed to be the value for
+            // the message in the default language. Defaults to true.
+            //i18n.LocalizedApplication.Current.MessageKeyIsValueInDefaultLanguage = false;
+
+            // Specifies a custom method called after a nugget has been translated
+            // that allows the resulting message to be modified, for instance according to content type.
+            // See [Issue #300](https://github.com/turquoiseowl/i18n/issues/300) for example usage case.
+            i18n.LocalizedApplication.Current.TweakMessageTranslation = delegate (System.Web.HttpContextBase context, i18n.Helpers.Nugget nugget, i18n.LanguageTag langtag, string message)
+            {
+                switch (context.Response.ContentType)
+                {
+                    case "text/html":
+                        return message.Replace("\'", "&apos;");
+                }
+                return message;
+            };
+
+            // Blacklist certain URLs from being 'localized' via a callback.
+            i18n.UrlLocalizer.IncomingUrlFilters += delegate (Uri url) {
+                if (url.LocalPath.EndsWith("sitemap.xml", StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+                return true;
+            };
+
+            // Extend (+=) or override (=) the default handler for Set-PAL event.
+            // The default handler applies the setting to both the CurrentCulture and CurrentUICulture
+            // settings of the thread, as shown below.
+            i18n.LocalizedApplication.Current.SetPrincipalAppLanguageForRequestHandlers = delegate (System.Web.HttpContextBase context, ILanguageTag langtag)
+            {
+                // Do own stuff with the language tag.
+                // The default handler does the following:
+                if (langtag != null)
+                {
+                    Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = langtag.GetCultureInfo();
+                }
+            };
+
+            // Blacklist certain URLs from being translated using a regex pattern. The default setting is:
+            //i18n.LocalizedApplication.Current.UrlsToExcludeFromProcessing = new Regex(@"(?:\.(?:less|css)(?:\?|$))|(?i:i18nSkip|glimpse|trace|elmah)");
+
+            // Whitelist content types to translate. The default setting is:
+            //i18n.LocalizedApplication.Current.ContentTypesToLocalize = new Regex(@"^(?:(?:(?:text|application)/(?:plain|html|xml|javascript|x-javascript|json|x-json))(?:\s*;.*)?)$");
+
+            // Change the types of async postback blocks that are localized
+            //i18n.LocalizedApplication.Current.AsyncPostbackTypesToTranslate = "updatePanel,scriptStartupBlock,pageTitle";
+
+            // Change which languages are parsed from the request, like skipping  the "Accept-Language"-header value. The default setting is:
+            //i18n.HttpContextExtensions.GetRequestUserLanguagesImplementation = (context) => LanguageItem.ParseHttpLanguageHeader(context.Request.Headers["Accept-Language"]);
+
+            // Override the i18n service injection. See source code for more details!
+            //i18n.LocalizedApplication.Current.RootServices = new Myi18nRootServices();
         }
     }
 }
